9a8be7b1ecb4db7090be430ec107094091e23810
Md Zafar <mz5659875@gmail.com>
2024-08-31 00:59:44 +0530

    Changes

--- models/userModel.js
+++ models/userModel.js
@@ -13,6 +13,9 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:[true, "Password  is required"]
     },
+    passwdResetToken: {
+        type: String
+    }
 },
 {
     timestamps:true
